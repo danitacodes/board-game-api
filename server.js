20bc9af6316b59aff7eaad12977b576ffad0721e
Danita Codes <danitacodes@gmail.com>
2022-06-06 12:28:05 -0700

    added more games to api

--- server.js
+++ server.js
@@ -20,6 +20,54 @@ let boardGame = {
         'minOfPlayers': 3,
         'maxOfPlayers': 6,
         'ageRange': '8 and up'
+    },
+    'candyland': {
+        'yearIntroduced': 1949,
+        'minOfPlayers': 2,
+        'maxOfPlayers': 4,
+        'ageRange': '3 and up'
+    },
+    'scrabble': {
+        'yearIntroduced': 1938,
+        'minOfPlayers': 2,
+        'maxOfPlayers': 4,
+        'ageRange': '8 and up'
+    },
+    'battleship': {
+        'yearIntroduced': 1967,
+        'minOfPlayers': 2,
+        'maxOfPlayers': 2,
+        'ageRange': '7 and up'
+    },
+    'risk': {
+        'yearIntroduced': 1957,
+        'minOfPlayers': 2,
+        'maxOfPlayers': 6,
+        'ageRange': '10 and up'
+    },
+    'stratego': {
+        'yearIntroduced': 1946,
+        'minOfPlayers': 2,
+        'maxOfPlayers': 2,
+        'ageRange': '8 and up'
+    },
+    'backgammon': {
+        'yearIntroduced': 1946,
+        'minOfPlayers': 2,
+        'maxOfPlayers': 2,
+        'ageRange': '5 and up'
+    },
+    'connect four': {
+        'yearIntroduced': 1974,
+        'minOfPlayers': 2,
+        'maxOfPlayers': 2,
+        'ageRange': '6 and up'
+    },
+    'operation': {
+        'yearIntroduced': 1965,
+        'minOfPlayers': 1,
+        'maxOfPlayers': 4,
+        'ageRange': '6 and up'
     }
 }
 //GET ROUTE
